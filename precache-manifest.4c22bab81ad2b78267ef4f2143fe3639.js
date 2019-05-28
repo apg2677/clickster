0c30595965e3ef629f33d471a8629b6ab297affc
Amir P <apourrezaei@gmail.com>
2019-05-28 18:27:49 -0400

    Updates

---
+++ precache-manifest.4c22bab81ad2b78267ef4f2143fe3639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9136ee4f7d4472389f7876e7c4bfc95b",
+    "url": "/clickster/index.html"
+  },
+  {
+    "revision": "e9c4eb4af162ff4dc05f",
+    "url": "/clickster/static/css/main.6efdad21.chunk.css"
+  },
+  {
+    "revision": "bb8d0ab5a75f1d496375",
+    "url": "/clickster/static/js/2.378a017d.chunk.js"
+  },
+  {
+    "revision": "e9c4eb4af162ff4dc05f",
+    "url": "/clickster/static/js/main.bbcd6769.chunk.js"
+  },
+  {
+    "revision": "40bb0c8c2e3a4a70d3d7",
+    "url": "/clickster/static/js/runtime~main.9928b7d5.js"
+  }
+]);
